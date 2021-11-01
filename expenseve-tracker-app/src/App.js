7e0b4475e5b2fcf9bb243d3a1118033d3d2d0f83
syedalihamdani <alihamdani61@yahoo.com>
2021-11-01 09:50:52 +0500

    Completed

--- expenseve-tracker-app/src/App.js
+++ expenseve-tracker-app/src/App.js
@@ -6,19 +6,20 @@ const App = () => {
   const [account,setaccount]=useState();
   const [networkName,setnetworkName]=useState("Wallet not");
   const [Balance,setBalance]=useState();
-  const [Screen,setScreen]=useState();
+  const [Balance2,setBalance2]=useState();
   const [Toaddress,setToaddress]=useState('j');
   const [Quantity,setQuantity]=useState();
-  const [Income,setIncome]=useState();
-  const [Expense,setExpense]=useState();
+  const [Income,setIncome]=useState(0.00);
+  const [Expense,setExpense]=useState(0.00);
  const connect = async () => {
       const Web3 = await getWeb3();
       setweb3(Web3);
       const accounts = await Web3.eth.getAccounts();
       setaccount(accounts[0]);
       const balance = await Web3.eth.getBalance(accounts[0]);
-      // console.log(balance);
-      setBalance(balance);
+      const balance2=Web3.utils.fromWei(balance,'ether');
+      // alert(balance2);
+      setBalance(balance2);
        const networkId = await Web3.eth.net.getId();
        if(networkId===1){
         setnetworkName("Mainnet")
@@ -44,18 +45,12 @@ const App = () => {
        
  }
 connect();
-const balance=async ()=>{
-  if(networkName!=="Rinkeby"){
-    alert("Connect to Rinkeby network otherwise it won't work")
-  }else{setScreen(Balance);
-  }
-}
 const toaddress=(event)=>{
   setToaddress(event.target.value);
 }
 const sendTransaction= async()=>{
   if(networkName!=="Rinkeby"){
-    alert("Connect to Rinkeby network otherwise it won't work")
+    alert("Connect your wallet and Connect to Rinkeby network otherwise it won't work")
   }else if(Toaddress.length!==42){
     alert("Enter the correct Rinkeby account address to see the allowance.copy the address from your wallet")
   }else{
@@ -64,7 +59,11 @@ const sendTransaction= async()=>{
   }
 }
 const setincome=(event)=>{
+  // console.log(event.target.value)
   setIncome(event.target.value);
+  // setIncome(Income);
+  // console.log(Income)
+  // setIncome2(String(Income));
 }
 const setexpense=(event)=>{
   setExpense(event.target.value);
@@ -74,10 +73,10 @@ const quantity=(event)=>{
 } 
 const equality=()=>{
   if(networkName!=="Rinkeby"){
-    alert("Connect to Rinkeby network otherwise it won't work")
+    alert("Connect your wallet and Connect to Rinkeby network otherwise it won't work")
   }else{
     const eq=Income-Expense;
-    setScreen(eq);
+    setBalance2(eq);
   }
 }
 const restart=()=>{
@@ -85,7 +84,7 @@ const restart=()=>{
 }
   return (
     <>
-  <div className="page">  
+  {/* <div className="page">  
   <nav className="navbar">
   <div className="upper">
       <h4>{networkName} Connected</h4>
@@ -106,9 +105,52 @@ const restart=()=>{
 <input className="input" placeholder="expense" type="number" onChange={setexpense}></input>
 <button className="btn2" onClick={()=>equality()}>Equality</button>
   </div>
-      
   </div>
+  </div> */}
+  <div className="body">
+  <nav className="navbar">
+  <div className="upper">
+      <h4>{networkName} Connected</h4>
+      <h4>Balance: {Balance}</h4>
+      <button className="btn" onClick={restart}><h4>Connect to Wallet</h4></button></div>
+  <div className="lower">Account:{account}</div>
+  </nav>
+  <h2>Expense Tracker</h2>
+  <div className="container">
+    <h4>Your Balance</h4>
+    <h1 id="balance">${Balance2}</h1>
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p className="money plus">+${Income}</p>
+        {/* <h3 className="c">{Income}</h3> */}
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p className="money minus">-${Expense}</p>
+      </div>
+    </div>
+    <h3>History</h3>
+    <h3>Add new transaction</h3>
+      <div className="form-control">
+        <h3> Transfer Ethers</h3>
+        {/* <input type="text" id="text" placeholder="Enter text..." /> */}
+<input className="input"  placeholder="To address" size="45" maxLength="42" type="string" onChange={toaddress}></input>
+<input className="input" placeholder="Amount in Ethers" type="number" onChange={quantity}></input>
+<button className="btn" onClick={()=>sendTransaction()}>Send Ethers </button>
+      </div>
+      <div className="form-control">
+        <label htmlFor="amount">Amount <br />
+        <h3>Amount</h3>
+          (negative - expense, positive - income)</label>
+        {/* <input type="number" id="amount" placeholder="Enter amount..." /> */}
+<input className="input" placeholder="INCOME" type="number" maxLength="42"  min="1" max="1000000000" onChange={setincome}></input>
+<input className="input" placeholder="EXPENSE" type="number" onChange={setexpense}></input>
+<button className="btn" onClick={()=>equality()}>Calculate</button>
+      </div>
   </div>
+</div>
+
     </>
   )
 }
--- expenseve-tracker-app/src/getWeb3.js
+++ expenseve-tracker-app/src/getWeb3.js
@@ -29,7 +29,7 @@ const getWeb3 = () =>
           "http://127.0.0.1:8545"
         );
         const web3 = new Web3(provider);
-        console.log("No web3 instance injected, using Local web3.");
+        alert("No web3 instance injected,Install Metamask wallet in your browser");
         resolve(web3);
       }
     });
